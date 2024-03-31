7e24e7c3a19a18c3a243aba875e3fa85c3fcd81e
jcy <jchengyu0829@163.com>
2024-03-31 08:55:42 +0800

    port=6060

--- jenkins/scripts/ci.sh
+++ jenkins/scripts/ci.sh
@@ -20,7 +20,12 @@ set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
+# 查找 application.properties 中包含 "server.port" 的行，并提取端口号
+port=$(grep "server.port" src/main/resources/application.properties | cut -d'=' -f2)
+
+echo "Spring Boot 应用端口号是: $port"
+
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-docker build --build-arg NAME="${NAME}" VERSION="${VERSION}" ../../
+docker build --build-arg NAME="${NAME}" VERSION="${VERSION}" PORT="${port}" ../../
